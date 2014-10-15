f7e8074881b42d0214855f403c755c39d68dd457
Matt Hall <matthall@Matts-MacBook-Pro.local>
2014-10-15 20:29:40 +0100

    Fuctional test now checkswe can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,14 +14,40 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn ('To-Do', self.browser.title)
+	
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+	
+		#She is invited to do a to-do item straight away
+	
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
+		
+		#She types 'Buy peacock features' into a text box
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+	
+	
+		#When she hits enter, the pages updates and now the page lists the to-do
+
+		#There is still a box inviting her to add another item 
+
+	
 		self.fail('Finish the test!')
 		
 		
  
-	#She is invited to do a to-do item straight away
-
-	#She types 'Buy peacock features' into a text box
-
+	
+	
 	#When she hits enter, the pages updates and now the page lists the to-do
 
 	#There is still a box inviting her to add another item 
